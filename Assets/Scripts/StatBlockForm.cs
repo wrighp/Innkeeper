6529433c6d497b508464e6c21756c2873e1bb208
molinh <molinari.hayden@gmail.com>
2018-03-26 04:04:42 -0400

    Update StatBlockForm.cs

--- Assets/Scripts/StatBlockForm.cs
+++ Assets/Scripts/StatBlockForm.cs
@@ -9,6 +9,22 @@ public class StatBlockForm : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+	/* rows holds an array of lineData structs
+         * For each line in the statblock file passed to the parser,
+         * a lineData struct is created which stores
+         *      words:       array of strings separated by tabs,
+         *                   modified to remove ` marks and [] brackets
+         *      weights:     a list object of ints containing the weights
+         *                   corresponding to the strings in words
+         *                   e.g. words[i] has a weight weights[i]
+         *      forms:       a list object of bools containing whether
+         *                   or not the corresponding string in words
+         *                   contained in an editable field
+         *                   e.g. words[i] having a field is forms[i]
+         *      totalWeight: the total weight of the line
+         *                   NOTE: totalWeight = -1 indicates an empty
+         *                   space
+         */
         List<lineData> rows = StatBlockParser.ReadData(Path);
         foreach(lineData obj in rows)
         {
