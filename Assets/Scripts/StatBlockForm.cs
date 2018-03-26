772240ac155229ca2f1ff8162eaeb2ca811154c3
molinh <molinari.hayden@gmail.com>
2018-03-25 22:19:13 -0400

    Please don't break

---
+++ Assets/Scripts/StatBlockForm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatBlockForm : MonoBehaviour {
+
+    public string Path;
+
+
+	// Use this for initialization
+	void Start () {
+        List<lineData> rows = StatBlockParser.ReadData(Path);
+        foreach(lineData obj in rows)
+        {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StatBlockParser.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+struct lineData
+{
+    public string[] words;
+    public List<int> weights;
+    public List<bool> forms;
+    public int totalWeight;
+
+    public lineData(string input)
+    {
+        totalWeight = 0;
+
+        words = input.Split('\t');
+        weights = new List<int>();
+        forms = new List<bool>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > 1 && words[i][0] == '/' && words[i][1] == '/')
+            {
+                Debug.Log("Comment");
+                break;
+            }
+            else if (i == 0 && words[i].Length > 0 && words[i][0] == '#')
+            {
+                Debug.Log("Hash");
+                totalWeight = -1;
+                break;
+            }
+            forms.Add(false);
+            if (words[i].Length > 0 && words[i][0] == '`')
+            {
+                weights.Add(1);
+                words[i] = words[i].Substring(1);
+            }
+            else if (words[i].Length > 0 && words[i][0] == '[' && words[i][(words[i].Length - 1)] == ']')
+            {
+                forms[i] = true;
+                weights.Add(1);
+                words[i] = words[i].Substring(1, words[i].Length - 2);
+            }
+            else weights.Add(3);
+            totalWeight += weights[i];
+            Debug.Log(words[i] + weights[i] + forms[i]);
+        }
+        Debug.Log("totalweight: " + totalWeight);
+    }
+
+};
+
+public static class StatBlockParser
+{
+    public static List<lineData> ReadData(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(@filename);
+        List<lineData> rows = new List<lineData>();
+        foreach(string line in lines) {
+            rows.Add(new lineData(line));
+        }
+
+        return rows;
+    }
+}
