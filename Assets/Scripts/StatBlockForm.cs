3ed911c3a7c7ae84a333252a1cfe1da1858009b7
molinh <molinari.hayden@gmail.com>
2018-03-26 04:05:48 -0400

    Update StatBlockForm.cs

--- Assets/Scripts/StatBlockForm.cs
+++ Assets/Scripts/StatBlockForm.cs
@@ -9,7 +9,7 @@ public class StatBlockForm : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	/* rows holds an array of lineData structs
+	/* rows holds a list object of lineData structs
          * For each line in the statblock file passed to the parser,
          * a lineData struct is created which stores
          *      words:       array of strings separated by tabs,
