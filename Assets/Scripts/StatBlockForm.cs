97761555cb4d5ee32c3d45f9096782fe6691875e
molinh <molinari.hayden@gmail.com>
2018-03-27 20:53:10 -0400

    Update StatBlockForm.cs

--- Assets/Scripts/StatBlockForm.cs
+++ Assets/Scripts/StatBlockForm.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatBlockForm : MonoBehaviour {
 
-    public string Path;
+    public TextAsset text;
+    public int stringWeight;
+    public int numWeight;
+    public int checkWeight;
 
-
-	// Use this for initialization
-	void Start () {
-	/* rows holds a list object of lineData structs
+    // Use this for initialization
+    void Start () {
+        /* rows holds a list object of lineData objectss
          * For each line in the statblock file passed to the parser,
          * a lineData struct is created which stores
          *      words:       array of strings separated by tabs,
@@ -17,16 +19,24 @@ public class StatBlockForm : MonoBehaviour {
          *      weights:     a list object of ints containing the weights
          *                   corresponding to the strings in words
          *                   e.g. words[i] has a weight weights[i]
-         *      forms:       a list object of bools containing whether
-         *                   or not the corresponding string in words
-         *                   contained in an editable field
+         *      forms:       a list object of WordType enums that correspond
+         *                   to each string in words:
+         *                      String:      a regular string
+         *                      StringInput: an alphanumeric input
+         *                      NumInput:    a numeric input
+         *                      Checked:     a checked checkbox
+         *                      Unchecked:   an unchecked checkbox
          *                   e.g. words[i] having a field is forms[i]
+         *      listing:     a ListType enum that denotes if this line
+         *                   of data is the start of a list, the end of
+         *                   a list, or neither
          *      totalWeight: the total weight of the line
          *                   NOTE: totalWeight = -1 indicates an empty
-         *                   space
+         *                   space. totalWeight = 0 indicates this line
+         *                   is not visible
          */
-        List<lineData> rows = StatBlockParser.ReadData(Path);
-        foreach(lineData obj in rows)
+        List<LineData> rows = StatBlockParser.ReadData(text, stringWeight, numWeight, checkWeight);
+        foreach(LineData obj in rows)
         {
 
         }
