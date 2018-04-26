64dd4a88083933f1e9f199664b0e045d04a7c7a8
John Steigerwald <johnstiggy@gmail.com>
2018-04-25 21:54:17 -0400

    :wrench: Changes to mapdata

---
+++ Assets/Scripts/MapForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapForm : PageObject {
+
+    public GameObject pin;
+
+    public override void BuildPage(PageData data)
+    {
+        MapData uiData = (MapData)data;
+
+        foreach (PinData p in uiData.pins)
+        {
+            GameObject tmp = Instantiate(pin, transform);
+            tmp.transform.position = p.position;
+        }
+
+        //GetComponent<Image>().sprite
+
+    }
+
+}
+
+[Serializable]
+public class MapData : PageData
+{
+    public string imagePath;
+    public PinData[] pins;
+
+    public MapData(GameObject g)
+    {
+        //Loop through transform for pins and create pindata
+        List<PinData> lP = new List<PinData>();
+        foreach (Pin p in g.GetComponentsInChildren<Pin>())
+        {
+            lP.Add(new PinData(p.gameObject));
+        }
+    }
+
+}
+
+[Serializable]
+public class PinData
+{
+    public string referencePath;
+    public Color32 color;
+    public Vector3 position;
+
+    public PinData(GameObject g)
+    {
+        //Take pin and convert to save data
+        RectTransform rect = g.GetComponent<RectTransform>();
+        position = rect.position;
+        color = (Color32)g.GetComponent<Image>().color;
+    }
+}
--- Assets/Scripts/PageObject.cs
+++ Assets/Scripts/PageObject.cs
@@ -17,37 +17,3 @@ public abstract class PageData
     //public abstract void OnAfterDeserialize();
     //public abstract void OnBeforeSerialize();
 }
-
-[Serializable]
-public class MapData : PageData
-{
-    public string imagePath;
-    public PinData[] pins;
-
-    public MapData(GameObject g)
-    {
-        //Loop through transform for pins and create pindata
-        List<PinData> lP = new List<PinData>();
-        foreach (Pin p in g.GetComponentsInChildren<Pin>())
-        {
-            lP.Add(new PinData(p.gameObject));
-        }
-    }
-
-}
-
-[Serializable]
-public class PinData
-{
-    public string referencePath;
-    public Color32 color;
-    public Vector3 position;
-
-    public PinData(GameObject g)
-    {
-        //Take pin and convert to save data
-        RectTransform rect = g.GetComponent<RectTransform>();
-        position = rect.position;
-        color = (Color32)g.GetComponent<Image>().color;
-    }
-}
